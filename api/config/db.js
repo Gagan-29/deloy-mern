4eae5d3999151b2ef072a8b68440e910eb3d175a
Gagan-29 <122251921+Gagan-29@users.noreply.github.com>
2024-07-22 22:18:59 +0530

    Update db.js

--- api/config/db.js
+++ api/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import colors from "colors";
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect('mongodb+srv://karnamgagan2923:gaganharshith@cluster0.rz259cd.mongodb.net/ecommerce');
     console.log(
       `Connected to database ${conn.connection.host}`.bgMagenta.white
     );
